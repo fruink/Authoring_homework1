8c9d5a2dab3db312b37eaa45dd3ba0539cbf172b
fruink <karlee23@live.com>
2017-12-22 11:26:03 -0500

    upload file

---
+++ fruin_k_authoring_hw1/script/contentObject.js
@@ -0,0 +1,122 @@
+var dynamicContent = {
+		spring: {
+			text : "Spring is one of the four conventional temperate seasons, following winter and preceding summer. There are various technical definitions of spring, but local usage of the term varies according to local climate, cultures and customs. When it is spring in the northern hemisphere, it will be autumn in the southern hemisphere. At the spring equinox, days are close to 12 hours long with day length increasing as the season progresses. Spring and 'springtime' refer to the season, and also to ideas of rebirth, rejuvenation, renewal, resurrection, and regrowth.",
+
+			headline : "Spring",
+
+			images : [
+				"spring_1.jpg",
+				"spring_2.jpg",
+				"spring_3.jpg",
+				"spring_4.jpg",
+				"spring_5.jpg",
+				"spring_6.jpg",
+				"spring_7.jpg",
+				"spring_8.jpg",
+				"spring_9.jpg",
+				"spring_10.jpg"
+			],
+
+			imageDescription : [
+				"Here are some cute bunnies",
+				"Here is a chick, he's so adorable!",
+				"These are some pretty birds on a branch",
+				"This is a branch covered with pink flowers",
+				"Here are some pretty pink butterflies",
+				"This is a happy goat",
+				"Here's some flowers and butterflies",
+				"Here are some random flowers",
+				"This happy couple likes spending time outdoors",
+				"Here are some pretty purple flowers"
+			]
+		},
+
+
+		summer : {
+			text : "Summer is the warmest of the four temperate seasons, falling between spring and autumn. At the summer solstice, the days are longest and the nights are shortest, with day-length decreasing as the season progresses after the solstice. The date of the beginning of summer varies according to climate, tradition and culture, but when it is summer in the Northern Hemisphere it is winter in the Southern Hemisphere, and vice versa.",
+
+			headline : "Summer",
+
+			images : [
+				"summer_1.jpg",
+				"summer_2.jpg",
+				"summer_3.jpg",
+				"summer_4.jpg",
+				"summer_5.jpg",
+				"summer_6.jpg",
+				"summer_7.jpg",
+				"summer_8.jpg",
+				"summer_9.jpg",
+				"summer_10.jpg"
+			],
+
+			imageDescription : [
+				"Here is a girl reading on the beach at sunset",
+				"This is a adorable little golden retreiver",
+				"This beach is really popular",
+				"Here are some friends lounging on a pizza pool floater",
+				"These are some kids dancing on the beach",
+				"Here are some random blow up pool floaters",
+				"This is a yellow flip flop in the sand, a coconut with an umbrella, and some cool blue sunglasses",
+				"Here are some palm trees on the beach",
+				"This little girl is playing in the sand on a beach",
+				"This is a dog with some awesome sunglasses, he look's great :)"
+			]
+		},
+
+		autumn : {
+			text : "Autumn, known as fall in the US and Canada,[1] is one of the four temperate seasons. Autumn marks the transition from summer into winter, in September (Northern Hemisphere) or March (Southern Hemisphere) when the arrival of night becomes noticeably earlier. One of its main features is the shedding of leaves from deciduous trees as they pave way for further growth. In North America, autumn is usually considered to start with the September equinox.[5] In traditional East Asian solar term, autumn starts on or around 8 August and ends on about 7 November. In Ireland, the autumn months according to the national meteorological service, Met Éireann, are September, October and November.[6] However, according to the Irish Calendar, which is based on ancient Gaelic traditions, autumn lasts throughout the months of August, September, and October, or possibly a few days later, depending on tradition. In Australia, autumn officially begins on 1 March and ends 31 May.[7]",
+
+			headline : "Autumn",
+
+			images : [
+				"autumn_1.jpg",
+				"autumn_2.jpg",
+				"autumn_3.jpg",
+				"autumn_4.jpg",
+				"autumn_5.jpg",
+				"autumn_6.jpg",
+				"autumn_7.jpg",
+				"autumn_8.jpg",
+				"autumn_9.jpg",
+				"autumn_10.jpg"
+			],
+
+			imageDescription : [
+				"This is a cute fox peaking out from a tree",
+				"This is a slithering snake",
+				"Here are some colorful leaves",
+				"Here is a train track covered in colorful leaves",
+				"Here are some red sneakers stepping on leaves",
+				"Anyone interested in ziplining with an incredible view?",
+				"These are steps leading to a colorful forest",
+				"This is a long colorful pathway",
+				"Here is a bench, anyone want to sit on me? :)",
+				"This is a beautiful open walkway covered in orange and red leaves"
+			]
+		},
+
+		winter : {
+			text : "Winter (/ˈwɪntər/) is the coldest season of the year in temperate climates, between autumn and spring. It is caused by the axis of the Earth in the respective hemisphere being oriented away from the Sun. Different cultures define different dates as the start of winter, and some use a definition based on weather, but when it is winter in the Northern Hemisphere it is summer in the Southern Hemisphere, and vice versa. In many regions, winter is associated with snow and freezing temperatures. At the winter solstice, the days are shortest and the nights are longest, with days lengthening as the season progresses after the solstice.",
+
+			headline : "Winter",
+
+			images : [
+				"winter_1.jpg",
+				"winter_2.jpg",
+				"winter_3.jpg",
+				"winter_4.jpg",
+				"winter_5.jpg",
+				"winter_6.jpg"
+			],
+
+			imageDescription : [
+				"This sunlight coming through some trees",
+				"This is a snowman",
+				"These are some people walking through the snow",
+				"These are some footprints in the snow",
+				"This is a frozen lake and a mountian",
+				"These are some skiers"
+			]
+		}
+	};
